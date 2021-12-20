b383471337d523dd2eeaa63427135eac5535bd5d
Maverick <Mav33987@gmail.com>
2021-12-19 20:27:28 -0600

    able to see current day forecast but setting up 5 day forecast with for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,16 @@ var wind = document.querySelector('.wind');
 var humidity = document.querySelector('.humidity');
 var button = document.querySelector('.submit');
 
+console.log('https://api.openweathermap.org/data/2.5/forecast?q=Austin&units=imperial&cnt=5&appid=5d525352fa588eed3d6d28ab5746dc12')
     button.addEventListener('click', function(name){
-        fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=5d525352fa588eed3d6d28ab5746dc12')
+        fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchName.value + '&units=imperial&cnt=5&appid=5d525352fa588eed3d6d28ab5746dc12')
         .then(response => response.json())
         .then(data => {
 
-            var tempValue = data['city']['temp'];
+            var cnt = 
+
+            for (var i = 0; i < cnt.length; i++) {
+            var tempValue = data['temp'];
             var nameValue = data['name'];
             var windValue = data['wind'];
             var humidityValue = data['humidity'];
@@ -19,9 +23,12 @@ var button = document.querySelector('.submit');
             temp.innerHTML = "Temp - "+ tempValue;
             wind.innerHTML = "Wind - "+ windValue;
             humidity.innerHTML = "Humidity - "+ humidityValue;
-            input.value ="";
+            searchName.value ="";
         
+            }
         })
         
-        .catch(err => alert("Couldn't Find City"));
+        .catch(err => console.log(err));
         })
+
+      
