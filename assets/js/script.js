f1a879199d09c2a341f6103bfac31dd42d92e2be
Maverick <Mav33987@gmail.com>
2021-12-19 21:29:51 -0600

    able to display current weather for single day

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,20 @@ var wind = document.querySelector('.wind');
 var humidity = document.querySelector('.humidity');
 var button = document.querySelector('.submit');
 
-console.log('https://api.openweathermap.org/data/2.5/forecast?q=Austin&units=imperial&cnt=5&appid=5d525352fa588eed3d6d28ab5746dc12')
-    button.addEventListener('click', function(name){
-        fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchName.value + '&units=imperial&cnt=5&appid=5d525352fa588eed3d6d28ab5746dc12')
+console.log('https://api.openweathermap.org/data/2.5/weather?q=Austin&units=imperial&appid=5d525352fa588eed3d6d28ab5746dc12')
+    button.addEventListener('click', function(citySearch){
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchName.value + '&units=imperial&appid=5d525352fa588eed3d6d28ab5746dc12')
         .then(response => response.json())
         .then(data => {
 
-            var cnt = 
+            
 
-            for (var i = 0; i < cnt.length; i++) {
-            var tempValue = data['temp'];
+            
+            var tempValue = data['main']['temp'];
             var nameValue = data['name'];
-            var windValue = data['wind'];
-            var humidityValue = data['humidity'];
+            var windValue = data['wind']['speed'];
+            var humidityValue = data['main']['humidity'];
+            
         
             city.innerHTML = nameValue;
             temp.innerHTML = "Temp - "+ tempValue;
@@ -25,7 +26,7 @@ console.log('https://api.openweathermap.org/data/2.5/forecast?q=Austin&units=imp
             humidity.innerHTML = "Humidity - "+ humidityValue;
             searchName.value ="";
         
-            }
+            
         })
         
         .catch(err => console.log(err));
